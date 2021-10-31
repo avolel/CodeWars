5d544e38ff932b577eb886306557723d40c6738d
Andre Volel <avolel@gmail.com>
2021-10-31 00:19:49 -0400

    Adding Big Numbers

---
+++ AddingBigNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace AddingBigNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"123 + 456 = {Add("123","456")}");
+        }
+
+        private static string Add(string number1, string number2)
+        {
+            number1 = (string.IsNullOrEmpty(number1)) ? "0" : number1;
+            number2 = (string.IsNullOrEmpty(number2)) ? "0" : number2;
+            return (BigInteger.Parse(number1) + BigInteger.Parse(number2)).ToString();
+        }
+    }
+}
