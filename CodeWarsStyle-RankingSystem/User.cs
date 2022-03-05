30a7a868c7cc5bd4166a0a124ffc52c0ebb861f2
Andre Volel <avolel@gmail.com>
2022-03-04 22:11:16 -0500

    code wars

---
+++ CodeWarsStyle-RankingSystem/User.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CodeWarsStyle_RankingSystem
+{
+    public class User
+    {
+        public int rank { get; set; } = -8;
+        public int progress { get; set; }
+        public void incProgress(int r)
+        {
+            int diffRank = rank - r;
+            int progression = ((10 * diffRank * diffRank) != 0);
+        }
+
+    }
+}
---
+++ SentenceSmash/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SentenceSmash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "hello", "world", "this", "is", "great" };
+            Console.WriteLine(Smash(words).Trim());
+        }
+
+        private static string Smash(string[] words)
+        {
+            return String.Join(" ", words);
+        }
+    }
+}
